c1a1e271eeac34e9e206d39684f29af693668739
Deadshotop <petrenko.andreiy@gmail.com>
2023-11-22 10:46:24 +0300

    new file:   app.js 	new file:   package-lock.json 	new file:   package.json 	new file:   views/Algeria.hbs 	new file:   views/Russia.hbs 	renamed:    index.html -> views/index.hbs 	new file:   views/partial/menu.hbs

---
+++ app.js
@@ -0,0 +1,34 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partial");
+
+app.get("/", function(request, response){ 
+    response.render("index", {
+        button_name: "submit"
+    })
+});
+
+app.use('/', bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post("/", function(request, response){ 
+    response.send("Form sent!");
+    console.dir(request.body);
+});
+
+app.get("/Russia", function(request, response){ 
+    response.render("Russia");
+});
+
+app.get("/Algeria", function(request, response){ 
+    response.render("Algeria");
+});  
+
+app.listen(3000, function(){
+    console.log("3000");
+});
